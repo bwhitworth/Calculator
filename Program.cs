3b486bf0e921a65bc97ef72266ad6a8b819e7234
Bethany Whitworth <bethanyjwhitworth@gmail.com>
2020-07-29 18:17:48 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type your letters and press Enter");
+            var inputLetters = Console.ReadKey();
+            Console.WriteLine("here you go:", inputLetters);
+        }
+    }
+}
